4cd11b3f675f2784b8fa38e637efce16ac2cdbef
Lenny <data@cforent.be>
2025-08-21 13:55:05 +0100

    Added class description, forced 'lower()' on necessary variables and resolved zipcode/country logic so it can default to 0000 or XX.

--- nbb_data/classes.py
+++ nbb_data/classes.py
@@ -1,4 +1,5 @@
 import os
+import re
 import uuid
 from datetime import datetime, timedelta
 
@@ -72,11 +73,21 @@ class NBBConnector:
 
 class References:
     """
+    Model dictionary to assemble data from references. The dictionaries under
+    references are formatted as the database table.
+
+    Attributes:
+        - enterprise_id (str)
+        - enterprise_name (str)
+        - legal_situation (str)
+        - references (all) (list[dict])
+        - initial_list (all INITIAL refrences) (list[dict])
+        - correction_list (all CORRECTION references) (list[dict])
     """
     def __init__(self, data: list):
-        self.enterprise_id = data[0]["EnterpriseNumber"]
-        self.enterprise_name = data[0].get("EnterpriseName")
-        self.legal_situation = data[0].get("LegalSituation")
+        self.enterprise_id = re.sub(r"\D", "", data[-1]["EnterpriseNumber"])
+        self.enterprise_name = data[-1].get("EnterpriseName")
+        self.legal_situation = data[-1].get("LegalSituation")
         self.references = [{
             "enterprise_id": self.enterprise_id,
             "start_date": datetime.strptime(
@@ -91,7 +102,10 @@ class References:
             "activity_code": d.get("ActivityCode"),
             "model_type": d["ModelType"],
             "last_update": datetime.today()
-            } for d in data]
+            }
+            for d in data
+            ]
+
         self.initial_list = []
         self.correction_list = []
         for ref_dict in self.references:
@@ -102,7 +116,20 @@ class References:
 
 
 class Filing:
-    """Represents a filing correspondending to a company."""
+    """
+    Represents a filing correspondending to a company.
+
+    Attributes:
+        - reference_number (str)
+        - enterprise_name (str)
+        - address (dict)
+        - legal_form (dict)
+        - joint_committies (list)
+        - rubrics (list[dict])
+        - administrators (dict)
+        - participating_interests (list[dict])
+        - shareholders (dict)
+    """
     def __init__(self, data: dict):
         self.reference_number: str | None = data.get("ReferenceNumber")
         self.enterprise_name: str | None = data.get("EnterpriseName")
@@ -117,45 +144,66 @@ class Filing:
 
 
 class Person:
-    """"""
-    def __init__(self, person, country_dict):
+    """
+    Represent a (natural) person. The description dictionary is formatted to
+    the database table. First_name, last_name and street have to be lowered
+    because they will uniquely identify a natural person.
+
+    Attributes:
+        - id (UUID)
+        - description (dict)
+
+    """
+    def __init__(self, person: dict, country_dict: dict):
         self.id = uuid.uuid4()
         self.description = {
             "person_id": self.id,
-            "first_name": person.get("FirstName"),
-            "last_name": person.get("LastName"),
-            "street": person["Address"].get("Street"),
+            "first_name":
+                person["FirstName"].lower()
+                if person.get("FirstName") else None,
+            "last_name":
+                person["LastName"].lower()
+                if person.get("LastName") else None,
+            "street":
+                person["Address"]["Street"].lower()
+                if person["Address"].get("Street") else None,
             "street_number": person["Address"].get("Number"),
-            "zipcode": person["Address"].get(
-                "City",
-                person["Address"].get("OtherPostalCode")
-                ).replace("pcd:m", ""),
-            "country_code": person["Address"].get(
-                "Country",
-                country_dict.get(
-                    person["Address"].get("OtherCountry", "XX")
-                )
-            ).replace("cty:m", "")
+            "zipcode":
+                person["Address"].get("City").replace("pcd:m", "")
+                if person["Address"].get("City")
+                else (person["Address"].get("OtherPostalCode") or "0000"),
+            "country_code":
+                person["Address"].get("Country").replace("cty:m", "")
+                if person["Address"].get("Country")
+                else country_dict.get(person["Address"].get("OtherCountry"))
+                or "XX",
         }
 
 
 class Entity:
-    """"""
+    """
+    Represent a legal person or entity. The description dictionary is formatted
+    to the database table.
+
+    Attributes:
+        - id (UUID)
+        - description (dict)
+    """
     def __init__(self, entity, country_dict):
         self.id = uuid.uuid4()
         self.description = {
-            "identifier": entity["Entity"].get("Identifier"),
-            "country_code": entity["Entity"]["Address"].get(
-                "Country",
-                country_dict.get(
-                    entity["Entity"]["Address"].get("OtherCountry", "XX")
-                )
-            ).replace("cty:m", ""),
-            "denomination": entity["Entity"].get("Name"),
-            "street": entity["Entity"]["Address"].get("Street"),
-            "street_number": entity["Entity"]["Address"].get("Number"),
-            "zipcode": entity["Entity"]["Address"].get(
-                "City",
-                entity["Entity"]["Address"].get("OtherPostalCode")
-            ).replace("pcd:m", "")
+            "identifier": self.id,
+            "entity_id": re.sub(r"\D", "", entity.get("Identifier")),
+            "country_code":
+                entity["Address"].get("Country").replace("cty:m", "")
+                if entity["Address"].get("Country")
+                else country_dict.get(entity["Address"].get("OtherCountry"))
+                or "XX",
+            "denomination": entity.get("Name"),
+            "street": entity["Address"].get("Street"),
+            "street_number": entity["Address"].get("Number"),
+            "zipcode":
+                entity["Address"].get("City").replace("pcd:m", "")
+                if entity["Address"].get("City")
+                else entity["Address"].get("OtherPostalCode") or "0000"
         }
