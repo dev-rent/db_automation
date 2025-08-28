1338551412728cd915fff39fcd4c69ad6ac075b4
Lenny <data@cforent.be>
2025-08-28 12:16:06 +0100

    Commit before creating new branch to clean data before database entry.

--- nbb_data/initial_pop.py
+++ nbb_data/initial_pop.py
@@ -29,22 +29,23 @@ from sqlalchemy.dialects.postgresql import insert
 
 from log_config import ScriptLogger
 from nbb_data.models import (
-    table_accounting_codes, table_company_info,
-    table_entities, table_facts, table_legal_persons, table_natural_persons,
-    table_part_int, table_shareholders, table_statements
+    table_accounting_codes, table_administrators_natural,
+    table_administrators_legal, table_company_info, table_entities,
+    table_facts, table_natural_persons, table_part_int, table_shareholders,
+    table_statements
 )
 from nbb_data.classes import NBBConnector, References, Filing, Person, Entity
 
-
+x = '1'
 start = time.time_ns()
 
 load_dotenv()
 
-pop_logger = ScriptLogger('logs/population.log', level=20)
+pop_logger = ScriptLogger(f"logs/population_{datetime.now()}.log", level=20)
 quit = "Quiting script..."
 pop_logger.log.info("Initialising log...")
 
-nbb = NBBConnector(echo=True)
+nbb = NBBConnector(echo=False)
 
 with nbb.engine.begin() as conn:
     query = conn.execute(text("SELECT dutch_name, a_2 FROM country_codes;"))
@@ -55,7 +56,7 @@ country_codes_dct = {
 }
 
 # Step 1:
-temp_references = "temp_references"
+temp_references = f"server{x}/temp_references"
 
 with os.scandir(temp_references) as it:
     ref_file_lst = [
@@ -66,8 +67,7 @@ with os.scandir(temp_references) as it:
 
 # 1.a Get basic company info
 for file in ref_file_lst[:1]:
-    file = "temp_references/0400032156.json"
-
+    file = "temp_references/0240277017.json"
     try:
         with open(file, 'r') as ref:
             references = References(json.load(ref))
@@ -110,9 +110,13 @@ for file in ref_file_lst[:1]:
         for d in references.initial_list
         ]
 
+    str_tempfolder = "server{}/temp_filing/{}.json"
     str_tempfolder = "temp_filing/{}.json"
 
     for tupl in filings_list:
+        # Every time a person or entity is captured, it needs to be comitted
+        # to the databse immediately within the same block. Both entities can
+        # appear in later blocks and need to be ready for update.
         try:
             with open(str_tempfolder.format(tupl[0]), 'r') as data:
                 filing = Filing(json.load(data))
@@ -123,15 +127,45 @@ for file in ref_file_lst[:1]:
         year = tupl[1]
 
         # 2.a Natural Persons
-        # Do not change because list needs to be empty for every filing
-        persons_list = []
-        # Exclusively for the next for-loop. If a persons appears in Natural
-        # Persons as well as Legal Persons, they can't be updated within the
-        # same transaction in the database. So they need to executed separatly.
+        # Do not change because list needs to be empty for every filing.
+        administrators_natural_list = []
         natural_persons_list = []
         for natural in filing.administrators["NaturalPersons"]:
             try:
                 temporary_person = Person(natural['Person'], country_codes_dct)
+                print(temporary_person.key)
+                base_dct = {
+                    "enterprise_id": references.enterprise_id,
+                    "person_id": temporary_person.id,
+                    "year": year
+                }
+                if natural["Mandates"]:
+                    for mandate in natural["Mandates"]:
+                        try:
+                            temp_dct = base_dct.copy()
+                            add_dct = {
+                                "function_code": mandate.get(
+                                    "FunctionMandate").replace("fct:m", ""),
+                                "start_date": datetime.strptime(
+                                    mandate["MandateDates"].get(
+                                        "StartDate"
+                                    ), "%Y-%m-%d"),
+                                "end_date": datetime.strptime(
+                                    mandate["MandateDates"].get(
+                                        "EndDate"
+                                    ), "%Y-%m-%d")
+                            }
+                        except Exception as e:
+                            pop_logger.log.error((
+                                f"Mandates: {references.enterprise_id}. "
+                                f"Error: {e}"
+                                ))
+                            continue
+
+                        temp_dct.update(add_dct)
+                        administrators_natural_list.append(temp_dct)
+                else:
+                    administrators_natural_list.append(base_dct)
             except Exception as e:
                 pop_logger.log.error((
                     "Whilst retrieving Natural persons for "
@@ -153,9 +187,32 @@ for file in ref_file_lst[:1]:
                         )
                 conn.execute(stmt)
 
+        if administrators_natural_list:
+            with nbb.engine.begin() as conn:
+                stmt_admin_nat = insert(
+                    table_administrators_natural
+                    ).values(
+                        administrators_natural_list
+                        )
+                stmt_admin_nat = stmt_admin_nat.on_conflict_do_update(
+                    index_elements=[
+                        "enterprise_id", "person_id"
+                    ],
+                    set_={
+                        "function_code": stmt_admin_nat.excluded.function_code,
+                        "start_date": stmt_admin_nat.excluded.start_date,
+                        "end_date": stmt_admin_nat.excluded.end_date,
+                        "year": stmt_admin_nat.excluded.year
+
+                    }
+                )
+        del natural_persons_list
+        del administrators_natural_list
+
         # 2.b Legal Persons
+        persons_list = []
         entities_list = []
-        legal_persons_list = []
+        administrators_legal_list = []
         for legal in filing.administrators["LegalPersons"]:
             try:
                 temporary_entity = Entity(legal["Entity"], country_codes_dct)
@@ -173,7 +230,8 @@ for file in ref_file_lst[:1]:
                 base_dct = {
                     "enterprise_id": references.enterprise_id,
                     "entity_id": temporary_entity.id,
-                    "natural_person": temporary_person.id,
+                    "person_id": temporary_person.id,
+                    "year": year
                 }
                 if legal["Mandates"]:
                     for mandate in legal["Mandates"]:
@@ -188,7 +246,7 @@ for file in ref_file_lst[:1]:
                                     ), "%Y-%m-%d"),
                                 "end_date": datetime.strptime(
                                     mandate["MandateDates"].get(
-                                        "StartDate"
+                                        "EndDate"
                                     ), "%Y-%m-%d")
                             }
                         except Exception as e:
@@ -199,12 +257,61 @@ for file in ref_file_lst[:1]:
                             continue
 
                         temp_dct.update(add_dct)
-                        legal_persons_list.append(temp_dct)
+                        administrators_legal_list.append(temp_dct)
                 else:
-                    legal_persons_list.append(base_dct)
+                    administrators_legal_list.append(base_dct)
+
+        if persons_list:
+            stmt = insert(table_natural_persons).values(persons_list)
+            stmt = stmt.on_conflict_do_update(
+                    index_elements=[
+                        "first_name", "last_name",
+                        "street", "street_number"
+                    ],
+                    set_={"person_id": stmt.excluded.person_id}
+                    )
+            with nbb.engine.begin() as conn:
+                conn.execute(stmt)
+            del stmt
+
+        if entities_list:
+            stmt = insert(table_entities).values(entities_list)
+            stmt = stmt.on_conflict_do_update(
+                    index_elements=[
+                        "entity_id", "country_code"
+                    ],
+                    set_={"identifier": stmt.excluded.identifier}
+                    )
+            with nbb.engine.begin() as conn:
+                conn.execute(stmt)
+            del stmt
+
+        if administrators_legal_list:
+            stmt_legal = insert(
+                table_administrators_legal
+                ).values(
+                    administrators_legal_list
+                    )
+            stmt_legal = stmt_legal.on_conflict_do_update(
+                index_elements=[
+                    "enterprise_id", "person_id"
+                ],
+                set_={
+                    "function_code": stmt_legal.excluded.function_code,
+                    "start_date": stmt_legal.excluded.start_date,
+                    "end_date": stmt_legal.excluded.end_date,
+                    "year": stmt_legal.excluded.year
+
+                }
+            )
+            with nbb.engine.begin() as conn:
+                conn.execute(stmt_legal)
+        del entities_list
+        del persons_list
 
         # 2.c Participating Interests
         part_int_list = []
+        entities_list = []
         for partint in filing.participating_interests:
             try:
                 temporary_entity = Entity(
@@ -250,8 +357,32 @@ for file in ref_file_lst[:1]:
                     f"PartIntHeld for {references.enterprise_id}. Error: {e}")
                 continue
 
+        if entities_list:
+            stmt = insert(table_entities).values(entities_list)
+            stmt = stmt.on_conflict_do_update(
+                    index_elements=[
+                        "entity_id", "country_code"
+                    ],
+                    set_={"identifier": stmt.excluded.identifier}
+                    )
+            with nbb.engine.begin() as conn:
+                conn.execute(stmt)
+            del stmt
+
+        if part_int_list:
+            stmt_part_int = insert(
+                table_part_int
+                ).values(
+                    part_int_list
+                    ).on_conflict_do_nothing()
+            with nbb.engine.begin() as conn:
+                conn.execute(stmt_part_int)
+        del entities_list
+
         # 2.d Shareholders
         shareholders_list = []
+        entities_list = []
+        persons_list = []
         if filing.shareholders.get("EntityShareHolders"):
             for entity in filing.shareholders["EntityShareHolders"]:
                 try:
@@ -294,6 +425,19 @@ for file in ref_file_lst[:1]:
                         ))
                     continue
 
+            if entities_list:
+                with nbb.engine.begin() as conn:
+                    stmt = insert(table_entities).values(entities_list)
+                    stmt = stmt.on_conflict_do_update(
+                            index_elements=[
+                                "entity_id", "country_code"
+                            ],
+                            set_={"identifier": stmt.excluded.identifier}
+                            )
+                    conn.execute(stmt)
+                del stmt
+            del entities_list
+
         if filing.shareholders.get("IndividualShareHolders"):
             for natural in filing.shareholders["IndividualShareHolders"]:
                 try:
@@ -335,6 +479,29 @@ for file in ref_file_lst[:1]:
                         ))
                     continue
 
+            if persons_list:
+                stmt = insert(table_natural_persons).values(persons_list)
+                stmt = stmt.on_conflict_do_update(
+                        index_elements=[
+                            "first_name", "last_name",
+                            "street", "street_number"
+                        ],
+                        set_={"person_id": stmt.excluded.person_id}
+                        )
+                with nbb.engine.begin() as conn:
+                    conn.execute(stmt)
+                del stmt
+            del persons_list
+
+        if shareholders_list:
+            stmt_share = insert(
+                table_shareholders
+                ).values(
+                    shareholders_list
+                    ).on_conflict_do_nothing()
+            with nbb.engine.begin() as conn:
+                conn.execute(stmt_share)
+
         # 2.e Rubrics
         codes = []
         rubrics = []
@@ -360,59 +527,6 @@ for file in ref_file_lst[:1]:
             continue
 
         # Step 3: Prepare available statements for execution
-        stmts_to_execute = []
-        if persons_list:
-            stmt_natural = insert(
-                table_natural_persons
-                ).values(
-                    persons_list
-                    )
-            stmt_natural = stmt_natural.on_conflict_do_update(
-                index_elements=[
-                    "first_name", "last_name", "street", "street_number"
-                ],
-                set_={"person_id": stmt_natural.excluded.person_id}
-                )
-            stmts_to_execute.append(stmt_natural)
-
-        if entities_list:
-            stmt_entities = insert(
-                table_entities
-                ).values(
-                    entities_list
-                    )
-            stmt_entities = stmt_entities.on_conflict_do_update(
-                index_elements=[
-                    "entity_id", "country_code"
-                ],
-                set_={"identifier": stmt_entities.excluded.identifier}
-            )
-            stmts_to_execute.append(stmt_entities)
-
-        if part_int_list:
-            stmt_part_int = insert(
-                table_part_int
-                ).values(
-                    part_int_list
-                    ).on_conflict_do_nothing()
-            stmts_to_execute.append(stmt_part_int)
-
-        if legal_persons_list:
-            stmt_legal = insert(
-                table_legal_persons
-                ).values(
-                    legal_persons_list
-                    ).on_conflict_do_nothing()
-            stmts_to_execute.append(stmt_legal)
-
-        if shareholders_list:
-            stmt_share = insert(
-                table_shareholders
-                ).values(
-                    shareholders_list
-                    ).on_conflict_do_nothing()
-            stmts_to_execute.append(stmt_share)
-
         stmt_codes = insert(
             table_accounting_codes
             ).values(
@@ -425,10 +539,15 @@ for file in ref_file_lst[:1]:
                 rubrics
                 ).on_conflict_do_nothing()
 
-        with nbb.engine.begin() as conn:
-            for stmt in stmts_to_execute:
-                conn.execute(stmt)
-            conn.execute(stmt_codes)
-            conn.execute(stmt_rubrics)
+        try:
+            with nbb.engine.begin() as conn:
+                conn.execute(stmt_codes)
+                conn.execute(stmt_rubrics)
+        except Exception as e:
+            pop_logger.log.error((
+                f"Failed inserting data for {references.enterprise_id} - "
+                f"{tupl[0]}. Error {e}"
+            ))
+            continue
 
 pop_logger.log.info(f"Finished in {(time.time_ns() - start)/1_000_000} ms.")
